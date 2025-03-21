c50ce1029ae6158f10910c1beba8d04cdb36f1f9
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2025-03-21 18:29:37 -0300

    feat: create router to access the ads dashboard

--- api/routes/utmify.ts
+++ api/routes/utmify.ts
@@ -1,70 +1,44 @@
-import puppeteer from 'puppeteer';
+import puppeteerExtra from 'puppeteer-extra';
+import puppeteerExtraPluginStealth from 'puppeteer-extra-plugin-stealth';
 import dotenv from 'dotenv';
 import { Router } from 'express';
+
+import { generateUrlWithProducts } from '../utils/helpers';
 
 dotenv.config();
 const router = Router();
 
 const login = process.env.UTM_LOGIN || '';
-const password = process.env.UTM_PASSWORD || '';
+let password = process.env.UTM_PASSWORD || '';
+password = password + '#'
 const link = process.env.UTM_LINK || '';
-const utmDashAutomacao = process.env.UTM_DASH_AUTOMACAO || '';
 
-router.get('/utmify/:day', async (req, res) => {
+// Ativando o plugin stealth
+puppeteerExtra.use(puppeteerExtraPluginStealth());
+
+router.get("/utmify/:day", async (req,res) => {
   const { day } = req.params;
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto(link, { waitUntil: 'networkidle2' });
+  const adsTableUrl = generateUrlWithProducts({ day });
 
-  await page.type('input[name="email"]', login);
-  await page.type('input[name="password"]', password);
-  await Promise.all([
-    page.click('button[type="submit"]'),
-    page.waitForNavigation({ waitUntil: 'networkidle2' })
-  ]);
-  console.log('✅ Login realizado com sucesso!');
+  try {
+    // Inicializando o browser com puppeteer-extra
+    const browser = await puppeteerExtra.launch({ headless: false }); // Define como false para visualizar o processo
+    const page = await browser.newPage();
+    await page.goto("https://app.utmify.com.br/login/");
+    await page.type('input[name="email"]', login);
+    await page.type('input[name="password"]', password);
+    await page.keyboard.press("Enter");
+    await page.waitForNavigation({ waitUntil: "networkidle2" });
+    // await page.screenshot({ path: "login.png" });
+    await page.goto(adsTableUrl);
+    await page.waitForNavigation({ waitUntil: "networkidle2" });
+    // await page.screenshot({ path: "dash.png" });
 
-  await page.goto(utmDashAutomacao, { waitUntil: 'networkidle2' });
+    await browser.close();
 
-  await page.waitForSelector('button:nth-child(4)', { visible: true });
-  await page.click('button:nth-child(4)');
-  console.log('✅ Dash de Anúncios acessado com sucesso!');
-
-  if (day === 'yesterday') {
-    await page.waitForSelector('.select2-selection', { visible: true });
-    await page.click('.select2-selection');
-
-    await page.waitForSelector('.select2-results__option', { visible: true });
-
-    const options = await page.$$('.select2-results__option');
-    for (const option of options) {
-      const text = await page.evaluate(el => el.textContent, option);
-      if (text === 'Ontem') {
-        await option.click();
-        console.log('✅ Selecionado o dia de ontem com sucesso!');
-        break;
-      }
-    }
+  } catch (error) {
+    console.log(error);
   }
-
-  await page.waitForSelector('.table', { visible: true });
-  const tableData: string[][] = [];
-
-  const rows = await page.$$('.table tr');
-  for (const row of rows) {
-    const cols = await row.$$('td, th');
-    const rowData = [];
-    for (const col of cols) {
-      const text = await page.evaluate(el => el.textContent?.trim(), col);
-      rowData.push(text || '');
-    }
-    tableData.push(rowData);
-  }
-
-  console.log('✅ Relatório carregado com sucesso!');
-  await browser.close();
-
-  res.json({ status: 'Sucesso', data: tableData });
 });
 
 export default router;
