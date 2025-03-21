728679a9eb25951c8877f29a3dfc45352b22863d
Raul Albuquerque <raulmalbuquerque2014@gmail.com>
2025-03-21 18:30:07 -0300

    feat: create all front products list

---
+++ api/utils/front_products_list.ts
@@ -0,0 +1,11 @@
+export const frontProducts = [
+  'Skin Toning Program 2.11',
+  'Cellulitis Free in 28 days 2.11',
+  'Skin Toning Program 2.1',
+  'Cellulitis Free in 28 days 2.1',
+  'Alpha App',
+  'Erecpower',
+  'Lemon Diet Program',
+  'EndoPrime',
+  'Overcoming Diabetes Program',
+]
